5c189b71543927214d2557ede649457e393c2eab
smt <andygood1357@gmail.com>
2026-02-03 10:48:57 +0900

    Updates

---
+++ _next/static/r87g5ATx9pP13FmdFUM1Z/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/smt//_next/:path+",
+        "destination": "/smt/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
